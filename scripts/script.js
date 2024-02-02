080af4aa79725483eaea9d72bfc7dfbedf886221
Victoria Goltseva <katnisseverdeen191@mail.ru>
2024-02-02 14:38:14 +0300

    animation

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+        stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+        typeSpeed: 100, // Скорость печати
+        startDelay: 500, // Задержка перед стартом анимации
+        backSpeed: 50, // Скорость удаления
+        loop: true // Указываем, повторять ли анимацию
+    });
+
+    new WOW().init();
+
+});
